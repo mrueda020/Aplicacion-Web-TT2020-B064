aeda8bf530b88bf6e9adee02e2a2d017fc3aecdc
mrueda020 <mrueda020@hotmail.com>
2021-08-26 21:00:49 -0500

    Exam Form

--- FrontEnd/src/components/Evaluador/ExamsForm/ExamsForm.js
+++ FrontEnd/src/components/Evaluador/ExamsForm/ExamsForm.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Transfer, Typography } from "antd";
+import {
+  Transfer,
+  Typography,
+  Form,
+  Switch,
+  DatePicker,
+  Button,
+  notification,
+} from "antd";
+import moment from "moment";
 import { fetchAllQuestions } from "../../../api/evaluador";
 import "./ExamsForm.scss";
 function ExamsForm() {
@@ -7,7 +16,8 @@ function ExamsForm() {
   const [questions, setQuestions] = useState([]);
   const [mockData, setMockData] = useState([]);
   const [targetKeys, setTargetKeys] = useState([]);
-
+  const [isMockExam, setIsMockExam] = useState(true);
+  const [dates, setDates] = useState([]);
   const cleanData = () => {
     const data = questions.map((q) => {
       const question = {
@@ -37,6 +47,63 @@ function ExamsForm() {
     setTargetKeys(newTargetKeys);
   };
 
+  const onChangeDatePicker = (value, dateString) => {
+    console.log("Selected Time: ", value);
+    console.log("Formatted Selected Time: ", dateString);
+    if (value === null) {
+      setDates([]);
+      return;
+    }
+    setDates(dateString);
+  };
+
+  const onFinish = () => {
+    let payload = {};
+    if (!targetKeys.length) {
+      notification["error"]({ message: "No has seleccionado las preguntas" });
+      return;
+    }
+    if (!isMockExam) {
+      if (!dates.length) {
+        console.log("here");
+        notification["error"]({
+          message: "Selecciona el periodo de aplicacion",
+        });
+        return;
+      }
+      console.log(dates);
+      console.log(targetKeys);
+      return;
+    }
+    console.log(targetKeys);
+  };
+  const range = (start, end) => {
+    const result = [];
+    for (let i = start; i < end; i++) {
+      result.push(i);
+    }
+    return result;
+  };
+
+  const disabledDate = (current) => {
+    // Can not select days before today and today
+    return current && current < moment().endOf("day");
+  };
+  const disabledRangeTime = (_, type) => {
+    if (type === "start") {
+      return {
+        disabledHours: () => range(0, 0),
+        disabledMinutes: () => range(0, 0),
+        disabledSeconds: () => [0, 0],
+      };
+    }
+    return {
+      disabledHours: () => range(0, 0),
+      disabledMinutes: () => range(0, 0),
+      disabledSeconds: () => [0, 0],
+    };
+  };
+
   return (
     <div className="ExamsForm">
       <Title level={2}>Crear Examen</Title>
@@ -50,6 +117,42 @@ function ExamsForm() {
         oneWay
       />
       <br />
+      <Form
+        onFinish={onFinish}
+        labelCol={{
+          span: 5,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+      >
+        <Form.Item label="Tipo de examen">
+          <Switch
+            defaultChecked
+            checkedChildren="Prueba"
+            unCheckedChildren="Evaluación"
+            onChange={(checked) => {
+              setIsMockExam(checked);
+              setDates([]);
+            }}
+          />
+        </Form.Item>
+        {!isMockExam && (
+          <Form.Item label="Periodo de aplicacion">
+            <DatePicker.RangePicker
+              showTime
+              disabledDate={disabledDate}
+              disabledTime={disabledRangeTime}
+              onChange={onChangeDatePicker}
+            />
+          </Form.Item>
+        )}
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Crear Examen
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
