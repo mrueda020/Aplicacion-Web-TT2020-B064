428413124e6558426a686116640addcada892294
mrueda020 <mrueda020@hotmail.com>
2021-12-06 21:14:42 -0600

    corregir mostrar examen vacio

--- FrontEnd/src/components/User/Evaluation/Evaluation.js
+++ FrontEnd/src/components/User/Evaluation/Evaluation.js
@@ -52,7 +52,7 @@ function Evaluation(props) {
   return (
     <>
       <div className="Evaluation">
-        {evaluationInfo.length > 0 ? (
+        {evaluationInfo.length > 0 && evaluation.length ? (
           <>
             {(moment(evaluationInfo[0].Exa_fecha_aplicacion_fin).valueOf() >
               moment.now() &&
